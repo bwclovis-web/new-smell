b343680100b43c407773b6410d27a258c4eb3461
Brian Clovis <bwclovis.web@gmail.com>
2025-05-10 09:32:14 -0400

    style

--- app/components/Atoms/RadioSelect/radioSelect-variants.ts
+++ app/components/Atoms/RadioSelect/radioSelect-variants.ts
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export type RadioSelectVariants = VariantProps<typeof radioSelectVariants>
-export const radioSelectVariants = cva([''], {
+export const radioSelectVariants = cva(['flex items-center justify-center'], {
   compoundVariants: [{}],
   defaultVariants: {},
   variants: {}
--- app/components/Organisms/SearchBar/SearchBar.tsx
+++ app/components/Organisms/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { type VariantProps } from 'class-variance-authority'
 import { type FC, type HTMLProps, type KeyboardEvent, useState } from 'react'
+import { NavLink } from 'react-router'
 
 import RadioSelect from '~/components/Atoms/RadioSelect/RadioSelect'
 import { styleMerge } from '~/utils/styleUtils'
@@ -11,11 +12,12 @@ interface SearchBarProps extends HTMLProps<HTMLDivElement>,
 
 const SearchBar: FC<SearchBarProps> = ({ className }) => {
   const [results, setResults] = useState([])
-  const [searchType, setSearchType] = useState('houses')
+  const [searchType, setSearchType] = useState('perfume-house')
+  const [searchValue, setSearchValue] = useState('')
 
   const handleKeyUp = async (evt: KeyboardEvent<HTMLInputElement>) => {
     const query = (evt.target as HTMLInputElement).value
-    const url = searchType === 'houses' ? '/api/perfume-houses' : '/api/perfume'
+    const url = searchType === 'perfume-house' ? '/api/perfume-houses' : '/api/perfume'
     const res = await fetch(`${url}?name=${encodeURIComponent(query)}`)
     const data = await res.json()
     setResults(data)
@@ -23,40 +25,47 @@ const SearchBar: FC<SearchBarProps> = ({ className }) => {
 
   const handleSelectType = evt => {
     setSearchType(evt.target.value)
+    setResults([])
+    setSearchValue('')
   }
 
   return (
     <>
-      <div className="flex flex-col">
-        <label htmlFor="search" className="sr-only">Search</label>
-        <input
-          type="text"
-          id="search"
-          placeholder="Search..."
-          onKeyUp={evt => {
-            handleKeyUp(evt)
-          }}
-          className={styleMerge(searchbarVariants({ className }))}
-        />
+      <div className="flex flex-col items-center relative">
         <RadioSelect
           handleRadioChange={evt => handleSelectType(evt)}
           data={[
-            { id: '1', name: 'type', type: 'radio', label: 'Perfume Houses', value: 'houses', defaultChecked: true },
-            { id: '2', name: 'type', type: 'radio', label: 'Perfumes', value: 'perfumes' }
+            { id: '1', name: 'type', type: 'radio', label: 'Perfume Houses', value: 'perfume-house', defaultChecked: true },
+            { id: '2', name: 'type', type: 'radio', label: 'Perfumes', value: 'perfume' }
           ]}
         />
-      </div>
-      {results.length > 0 && (
-        <div className="absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg mt-1 max-h-60 overflow-y-auto">
-          <ul className="divide-y divide-gray-200">
+        <div className="flex gap-2 w-full">
+          <label htmlFor="search" className="sr-only">Search</label>
+          <input
+            type="text"
+            id="search"
+            onChange={evt => setSearchValue(evt.target.value)}
+            value={searchValue}
+            placeholder="Search..."
+            onKeyUp={evt => {
+              handleKeyUp(evt)
+            }}
+            className={styleMerge(searchbarVariants({ className }))}
+          />
+        </div>
+        {results.length > 0 && (
+          <ul className="bg-white rounded-b-md absolute w-full -bottom-10">
             {results.map((item: any) => (
-              <li key={item.id} className="p-2 hover:bg-gray-100 cursor-pointer">
-                {item.name}
+              <li key={item.id} className="p-2 hover:bg-noir-gray hover:text-noir-light cursor-pointer last-of-type:rounded-b-md">
+                <NavLink to={`${searchType}/${item.name}`} className="block w-full h-full">
+                  {item.name}
+                </NavLink>
               </li>
             ))}
           </ul>
-        </div>
-      )}
+        )}
+      </div>
+
     </>
 
   )
