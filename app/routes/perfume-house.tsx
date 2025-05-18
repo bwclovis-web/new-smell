3f739c8c50927f4bdea363ab220eaa041cd32da4
Brian Clovis <bwclovis.web@gmail.com>
2025-05-18 12:23:24 -0400

    add edit button

--- app/routes/perfume-house.tsx
+++ app/routes/perfume-house.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { GrEdit } from 'react-icons/gr'
 import { MdDeleteForever } from 'react-icons/md'
 import { type LoaderFunctionArgs, type MetaFunction, NavLink, useLoaderData, useNavigate } from 'react-router'
 
@@ -51,17 +52,21 @@ const HouseDetailPage = () => {
               {perfumeHouse.founded}
             </p>
           </div>
-          <button onClick={() => handleDelete()} aria-label="delete" className="bg-red-600/60 hover:bg-red-600/90 rounded-full p-2 cursor-pointer border-2 border-red-600/60 hover:border-red-600/90 transition-all duration-300 ease-in-out">
-            <MdDeleteForever size={40} fill="white" />
-          </button>
-          <NavLink
-            viewTransition
-            to={`/admin/perfume-house/${perfumeHouse.name}/edit`}
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-          >
-            Edit
-          </NavLink>
-        </div>
+          <div className="flex gap-4 items-center">
+            <button onClick={() => handleDelete()} aria-label={`delete ${perfumeHouse.name}`} className="bg-red-600/60 hover:bg-red-600/90 rounded-full p-2 cursor-pointer border-2 border-red-600/60 hover:border-red-600/90 transition-all duration-300 ease-in-out">
+              <MdDeleteForever size={40} fill="white" />
+            </button>
+            <NavLink
+              aria-label={`edit ${perfumeHouse.name}`}
+              viewTransition
+              to={`/admin/perfume-house/${perfumeHouse.name}/edit`}
+              className="bg-blue-600/60 p-3 hover:bg-blue-600/90 text-white rounded-full  flex items-center justify-center border-2 border-blue-600/60 hover:border-blue-600 transition-all duration-300 ease-in-out"
+            >
+              <GrEdit size={32} fill="white" />
+            </NavLink>
+          </div>
+        </div
+        >
       </header>
       <div className="flex gap-20">
         <div className="w-1/2 noir-outline rounded-b-lg">
