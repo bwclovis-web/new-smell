b0abf8994a0a8b54e3ab8968ae221b72c5c1842f
Brian Clovis <bwclovis.web@gmail.com>
2025-06-07 14:01:34 -0400

    fix z index

--- app/routes/admin/AdminLayout.tsx
+++ app/routes/admin/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 import AdminNavigation from '~/components/Molecules/AdminNavigation/AdminNavigation'
 
 const AdminLayout = () => (
-  <div className="flex flex-col md:flex-row w-full gap-4 md:gap-10 items-start">
+  <div className="flex flex-col md:flex-row w-full gap-4 md:gap-10 items-start relative z-10">
     <AdminNavigation />
     <div className="flex-1 p-4 w-full">
       <Outlet />
