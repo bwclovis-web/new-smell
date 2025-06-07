1a187ab2c8fbf76bfecd6e82416ea55fab0b05b0
Brian Clovis <bwclovis.web@gmail.com>
2025-06-07 08:28:05 -0400

    env updates

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,6 @@ import { vercelPreset } from '@vercel/react-router/vite'
 
 export default {
   ssr: true,
-  presets: [vercelPreset()]
+  presets: [vercelPreset()],
+  serverBuildPath: 'build/server/index.js'
 } satisfies Config
