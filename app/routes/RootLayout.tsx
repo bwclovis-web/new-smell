30aae21f7b776cebe4d2f2b03b9a54b0c8dc7b42
Brian Clovis <bwclovis.web@gmail.com>
2025-06-07 09:09:35 -0400

    update root

--- app/routes/RootLayout.tsx
+++ app/routes/RootLayout.tsx
@@ -2,20 +2,27 @@ import { type LoaderFunctionArgs, Outlet, useLoaderData } from 'react-router'
 
 import GlobalNavigation from '~/components/Molecules/GlobalNavigation/GlobalNavigation'
 import { getUserById } from '~/models/user.server'
+import { parseCookies, verifyJwt } from '../../api/utils'
 
-export const loader = async ({ context }: LoaderFunctionArgs) => {
-  console.log('RootLayout loader called ==============================>', context)
-  const user = context?.user?.id
-    ? await getUserById(context?.user?.id)
-    : null
-
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  // Get cookies from the request
+  const cookieHeader = request.headers.get('cookie') || ''
+  const cookies = parseCookies({ headers: { cookie: cookieHeader } })
+  let user = null
+  if (cookies.token) {
+    const payload = verifyJwt(cookies.token)
+    if (payload && payload.userId) {
+      user = await getUserById(payload.userId)
+    }
+  }
   return { user }
 }
+
 const RootLayout = () => {
   const { user } = useLoaderData<typeof loader>()
   return (
     <div className="flex flex-col gap-8 items-center min-h-full bg-noir-light  px-4 relative h-full">
-      <GlobalNavigation user={user} />
+      <GlobalNavigation />
       <main className="w-full md:w-3/4 h-full">
         <Outlet
           context={{ user }}
