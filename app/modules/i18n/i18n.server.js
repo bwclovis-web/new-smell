c940ac560be97b1b1e5816b030d24c2465d8a389
Brian Clovis <bwclovis.web@gmail.com>
2025-06-08 11:12:47 -0400

    fix top level error

--- app/modules/i18n/i18n.server.js
+++ app/modules/i18n/i18n.server.js
@@ -3,6 +3,7 @@ import Backend from 'i18next-fs-backend'
 import { LanguageDetector } from 'i18next-http-middleware'
 import path from 'path'
 import { initReactI18next } from 'react-i18next'
+import fs from 'node:fs' // Use node:fs directly instead of dynamic import
 
 i18n
   .use(Backend)
@@ -16,7 +17,14 @@ i18n
       escapeValue: false // React already does escaping
     },
     backend: {
-      loadPath: path.resolve('./public/locales/{{lng}}/{{ns}}.json')
+      loadPath: path.resolve('./public/locales/{{lng}}/{{ns}}.json'),
+      // Add fs instance to avoid dynamic import
+      readFile: (filename, callback) => {
+        fs.readFile(filename, 'utf8', callback)
+      },
+      writeFile: (filename, data, callback) => {
+        fs.writeFile(filename, data, 'utf8', callback)
+      }
     },
     detection: {
       order: ['cookie', 'header'],
