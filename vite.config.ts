7519b57c03ca27a050bb3bf7f161859d4de0e8a7
Brian Clovis <bwclovis.web@gmail.com>
2025-06-08 11:12:47 -0400

    fix top level error

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths(), reactRouter()]
+  plugins: [tailwindcss(), tsconfigPaths(), reactRouter()],
+  build: {
+    target: 'es2022', // Ensure target supports top-level await
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022', // Specify ESBuild target to support top-level await
+    }
+  }
 })
