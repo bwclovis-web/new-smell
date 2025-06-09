76c6f481264126c7b51193390a8693a4c3f574e5
Brian Clovis <bwclovis.web@gmail.com>
2025-06-09 07:58:19 -0400

    fix perfume edit

--- app/utils/sharedLoader.ts
+++ app/utils/sharedLoader.ts
@@ -1,27 +1,30 @@
-import { redirect } from 'react-router';
-import { parseCookies, verifyJwt } from '@api/utils';
-import { getUserById } from '~/models/user.server';
-import { createSafeUser } from '~/types';
+/* eslint-disable max-statements */
+import { parseCookies, verifyJwt } from '@api/utils'
+import { redirect } from 'react-router'
+
+import { getUserById } from '~/models/user.server'
+import { ROUTE_PATH as SIGN_IN } from '~/routes/login/SignInPage'
+import { createSafeUser } from '~/types'
 
 export const sharedLoader = async (request: Request) => {
-  const cookieHeader = request.headers.get('cookie') || '';
-  const cookies = parseCookies({ headers: { cookie: cookieHeader } });
+  const cookieHeader = request.headers.get('cookie') || ''
+  const cookies = parseCookies({ headers: { cookie: cookieHeader } })
 
   if (!cookies.token) {
-    throw redirect('/sign-in');
+    throw redirect(SIGN_IN)
   }
 
-  const payload = verifyJwt(cookies.token);
+  const payload = verifyJwt(cookies.token)
   if (!payload || !payload.userId) {
-    throw redirect('/sign-in');
+    throw redirect('/sign-in')
   }
 
-  const fullUser = await getUserById(payload.userId);
-  const user = createSafeUser(fullUser);
+  const fullUser = await getUserById(payload.userId)
+  const user = createSafeUser(fullUser)
 
   if (!user) {
-    throw redirect('/sign-in');
+    throw redirect('/sign-in')
   }
 
-  return user;
-};
+  return user
+}
