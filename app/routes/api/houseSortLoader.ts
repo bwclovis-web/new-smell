5faf7bc5476531321dc485d218e9890a30cbafcd
Brian Clovis <bwclovis.web@gmail.com>
2025-06-29 13:48:33 -0400

    add filter

--- app/routes/api/houseSortLoader.ts
+++ app/routes/api/houseSortLoader.ts
@@ -5,12 +5,15 @@ import { getAllHouses } from '~/models/house.server'
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url)
   const sortByType = url.searchParams.get('sortByType')
+  const houseType = url.searchParams.get('houseType')
+  const sortBy = url.searchParams.get('sortBy')
 
-  if (sortByType === null) {
-    return []
+  const options = {
+    sortByType: sortByType === 'true',
+    houseType: houseType || 'all',
+    sortBy: sortBy as 'name-asc' | 'name-desc' | 'created-desc' | 'created-asc' | 'type-asc' | undefined
   }
 
-  const sortByTypeBool = sortByType === 'true' ? true : sortByType === 'false' ? false : undefined
-  const result = await getAllHouses(sortByTypeBool)
-  return result ? result : []
+  const result = await getAllHouses(options)
+  return result || []
 }
