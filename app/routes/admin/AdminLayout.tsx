0286e5d0dcc148187d00f97c19d6254ec3b16405
Brian Clovis <bwclovis.web@gmail.com>
2025-06-29 11:17:00 -0400

    wip

--- app/routes/admin/AdminLayout.tsx
+++ app/routes/admin/AdminLayout.tsx
@@ -14,7 +14,7 @@ const AdminLayout = () => {
 
   return (
     <div className="flex flex-col md:flex-row w-full gap-4 md:gap-10 items-start relative z-10">
-      {user.role === 'admin' && <AdminNavigation />}
+      {user && <AdminNavigation user={user} />}
       <div className="flex-1 p-4 w-full">
         <Outlet context={{ user }} />
       </div>
